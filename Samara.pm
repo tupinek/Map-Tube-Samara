cf05d0e8be6639248f9dda8f76104ae586651fdf
Michal Špaček <tupinek@gmail.com>
2015-01-08 21:09:36 +0100

    Add Map::Tube::GraphViz, Map::Tube::Text::Table, Task::Map::Tube, Map::Tube::Bucharest, Map::Tube::Budapest, Map::Tube::Dnipropetrovsk, Map::Tube::Kazan, Map::Tube::Kharkiv, Map::Tube::Nanjing, Map::Tube::Novosibirsk, Map::Tube::SaintPetersburg, Map::Tube::Tbilisi, Map::Tube::Vienna and Map::Tube::Yekaterinburg to SEE ALSO section in doc.

--- Samara.pm
+++ Samara.pm
@@ -157,18 +157,33 @@ L<namespace::clean>.
 =head1 SEE ALSO
 
 L<Map::Tube>,
+L<Map::Tube::GraphViz>,
+L<Map::Tube::Text::Table>,
+L<Task::Map::Tube>.
+
 L<Map::Tube::Barcelona>,
 L<Map::Tube::Berlin>,
+L<Map::Tube::Bucharest>,
+L<Map::Tube::Budapest>,
 L<Map::Tube::Delhi>,
-L<Map::Tube::London>,
+L<Map::Tube::Dnipropetrovsk>,
+L<Map::Tube::Kazan>,
+L<Map::Tube::Kharkiv>,
 L<Map::Tube::Kiev>,
+L<Map::Tube::London>,
 L<Map::Tube::Minsk>,
 L<Map::Tube::Moscow>,
 L<Map::Tube::NYC>,
+L<Map::Tube::Nanjing>,
+L<Map::Tube::Novosibirsk>,
 L<Map::Tube::Prague>,
+L<Map::Tube::SaintPetersburg>,
 L<Map::Tube::Sofia>,
+L<Map::Tube::Tbilisi>,
 L<Map::Tube::Tokyo>,
-L<Map::Tube::Warsaw>.
+L<Map::Tube::Vienna>,
+L<Map::Tube::Warsaw>,
+L<Map::Tube::Yekaterinburg>.
 
 =head1 REPOSITORY
 
