094999bbb63832d63ec1b11fdcaff039c58e0b59
Michal Josef Špaček <tupinek@gmail.com>
2017-11-22 08:43:18 +0100

    Prepare for 0.08 version

--- Samara.pm
+++ Samara.pm
@@ -11,7 +11,7 @@ use Moo;
 use namespace::clean;
 
 # Version.
-our $VERSION = 0.07;
+our $VERSION = 0.08;
 
 # Get XML.
 has xml => (
@@ -304,6 +304,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.07
+0.08
 
 =cut
